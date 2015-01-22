5852f6911b9e617de506560e4456eff9bb441a4b
Vanya Petkova <vanyapetkova93@abv.bg>
2015-01-22 19:20:31 +0000

    Adding '\r' to the end of all serial commands
    We need that so that they can be truely executed.

--- Java-Communication/Communication/src/com/sdp/RobotCommunication.java
+++ Java-Communication/Communication/src/com/sdp/RobotCommunication.java
@@ -13,94 +13,94 @@ public class RobotCommunication implements ArduinoCommunication {
 
 	@Override
 	public void sendMove(int leftSpeed, int rightSpeed) {
-		String command = String.format("A_SET_ENGINE %d %d", leftSpeed,
+		String command = String.format("A_SET_ENGINE %d %d\r", leftSpeed,
 				rightSpeed);
 		Communication.getInstance().sendCommandViaPort(command);
 	}
 
 	@Override
 	public void sendCatch(int catchPosition) {
-		String command = String.format("A_SET_CATCH %d", catchPosition);
+		String command = String.format("A_SET_CATCH %d\r", catchPosition);
 		Communication.getInstance().sendCommandViaPort(command);
 	}
 
 	@Override
 	public void sendKick(int kickPosition) {
-		String command = String.format("A_SET_KICK %d", kickPosition);
+		String command = String.format("A_SET_KICK %d\r", kickPosition);
 		Communication.getInstance().sendCommandViaPort(command);
 
 	}
 
 	@Override
 	public void sendFKick(int kickPosition) {
-		String command = String.format("A_SET_FKICK %d", kickPosition);
+		String command = String.format("A_SET_FKICK %d\r", kickPosition);
 		Communication.getInstance().sendCommandViaPort(command);
 
 	}
 
 	@Override
 	public void sendGrab(int grabPosition) {
-		String command = String.format("A_SET_GRAB %d", grabPosition);
+		String command = String.format("A_SET_GRAB %d\r", grabPosition);
 		Communication.getInstance().sendCommandViaPort(command);
 	}
 
 	@Override
 	public void sendShoot(int shootPosition) {
-		String command = String.format("A_SET_SHOOT %d", shootPosition);
+		String command = String.format("A_SET_SHOOT %d\r", shootPosition);
 		Communication.getInstance().sendCommandViaPort(command);
 	}
 
 	@Override
 	public void sendTShoot(int shootPosition) {
-		String command = String.format("A_SET_TSHOOT %d", shootPosition);
+		String command = String.format("A_SET_TSHOOT %d\r", shootPosition);
 		Communication.getInstance().sendCommandViaPort(command);
 	}
 
 	@Override
 	public void sendStartMoveUp() {
-		String command = "A_START_MOVE_UP";
+		String command = "A_START_MOVE_UP\r";
 		Communication.getInstance().sendCommandViaPort(command);
 	}
 
 	@Override
 	public void sendStopMoveUp() {
-		String command = "A_STOP_MOVE_UP";
+		String command = "A_STOP_MOVE_UP\r";
 		Communication.getInstance().sendCommandViaPort(command);
 	}
 
 	@Override
 	public void sendStartMoveDown() {
-		String command = "A_START_MOVE_DOWN";
+		String command = "A_START_MOVE_DOWN\r";
 		Communication.getInstance().sendCommandViaPort(command);
 	}
 
 	@Override
 	public void sendStopMoveDown() {
-		String command = "A_STOP_MOVE_DOWN";
+		String command = "A_STOP_MOVE_DOWN\r";
 		Communication.getInstance().sendCommandViaPort(command);
 	}
 
 	@Override
 	public void sendStartTurnRight() {
-		String command = "A_START_TURN_RIGHT";
+		String command = "A_START_TURN_RIGHT\r";
 		Communication.getInstance().sendCommandViaPort(command);
 	}
 
 	@Override
 	public void sendStopTurnRight() {
-		String command = "A_STOP_TURN_RIGHT";
+		String command = "A_STOP_TURN_RIGHT\r";
 		Communication.getInstance().sendCommandViaPort(command);
 	}
 
 	@Override
 	public void sendStartTurnLeft() {
-		String command = "A_START_TURN_LEFT";
+		String command = "A_START_TURN_LEFT\r";
 		Communication.getInstance().sendCommandViaPort(command);
 	}
 
 	@Override
 	public void sendStopTurnLeft() {
-		String command = "A_STOP_TURN_LEFT";
+		String command = "A_STOP_TURN_LEFT\r";
 		Communication.getInstance().sendCommandViaPort(command);
 	}
 }
