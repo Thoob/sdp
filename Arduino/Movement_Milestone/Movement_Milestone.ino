fae7c8a201871f6561e967940fbe138ee3ed3c1b
Vanya Petkova <vanyapetkova93@abv.bg>
2015-01-24 11:47:10 +0000

    Adding move special, catch, kick, turn....
    They will change soon !

--- Arduino/Movement_Milestone/Movement_Milestone.ino
+++ Arduino/Movement_Milestone/Movement_Milestone.ino
@@ -3,8 +3,9 @@
 #include "Wire.h"
 
 // Speed Constants
+int MIN_SPEED = 30;
+int MID_SPEED = 50;
 int MAX_SPEED = 100;
-int MIN_SPEED = 10;
 
 // Communications
 SerialCommand comm;
@@ -16,17 +17,17 @@ void setup(){
   comm.addCommand("A_STOP", stop_motor);
   comm.addCommand("A_STOP_ALL", stop_all);
   
+  comm.addCommand("A_MOVE", move_special);
   comm.addCommand("A_MOVE_UP", move_up);
   comm.addCommand("A_MOVE_DOWN", move_down);
   
   comm.addCommand("A_ROTATE", rotate);
-
-  comm.addCommand("A_TURN_LEFT", turn_left);
-  comm.addCommand("A_TURN_RIGHT", turn_right);
+  
+  comm.addCommand("A_TURN", set_turn);
   
   comm.addCommand("A_SET_CATCH", set_catch);
   comm.addCommand("A_SET_KICK", set_kick);
-  comm.addCommand("A_SET_GRAB", set_grab);
+  
   comm.addCommand("A_SET_SHOOT", set_shoot);
 
 }
@@ -36,7 +37,7 @@ void loop(){
   comm.readSerial();
   
 }
-
+ 
 void stop_motor()
 {
   char *motor_num_in;
@@ -51,10 +52,64 @@ void stop_motor()
   
 }
 
+void stop_motor_local(int motor_num)
+{
+    motorStop(motor_num);
+  
+}
+
 void stop_all()
 {
   motorAllStop();
 
+}
+
+void move_special()
+{
+  char *speed_left;
+  char *speed_right;  
+  char *direction_left;
+  char *direction_right;
+  
+  int speed_l;
+  int speed_r;
+  int direction_l;
+  int direction_r;
+  
+  direction_left = comm.next();
+  speed_left = comm.next();
+  
+  direction_right = comm.next();
+  speed_right = comm.next();
+  
+  if(speed_left != NULL && speed_right != NULL && direction_left != NULL && direction_right != NULL)
+  {
+    speed_l = atof(speed_left);
+    speed_r = atof(speed_right);
+    direction_l = atof(direction_left);
+    direction_r = atof(direction_right);
+    
+    if(direction_l == 1)
+    {
+      motorForward(1, speed_l);
+    }
+    if(direction_r == 1)
+    {
+      motorForward(2, speed_r);
+    }
+    if(direction_l == 2)
+    {
+      motorBackward(1, speed_l);
+    }
+    if(direction_r == 2)
+    {
+      motorBackward(2, speed_r);
+    }
+    
+  }
+  
+  
+  
 }
 
 void move_up()
@@ -96,12 +151,12 @@ void set_move(int dir)
     if (right_speed > MAX_SPEED)
     {
       right_speed = MAX_SPEED;
-    }
+    } 
     
     /*Check whether we are moving up or down */
-    if(dir == 1)
+     if(dir == 1)
     {
-      /*motorForward(motorNum, motorPower)*/
+
       motorForward(1, left_speed); /* set the power of the left motor (num 1)*/
       motorForward(2, right_speed); /* set the power of the right motor (num 2)*/
     }
@@ -117,31 +172,15 @@ void set_move(int dir)
   } 
 }
 
-void turn_left()
+void set_turn()
 {
-  set_turn(1);
+    rotate();
+    delay(400);
+    stop_all();
 }
 
-void turn_right()
-{
-  set_turn(2);
-}
-
-/*if direction = 1 => LEFT, if direction = 2 => RIGHT*/
-void set_turn(int dir)
-{
-  /* TURN LEFT */
-  if(dir == 1)
-  {
-  }
-  
-  /* TURN RIGHT */
-  if(dir == 2)
-  {
-  }
-}
-
-void rotate(){    
+void rotate()
+{    
   
   char *dir_in;
   int dir = 1;
@@ -164,16 +203,32 @@ void rotate(){
 
 void set_catch()
 {
-  
+  motorForward(3, MIN_SPEED);
+  delay(400);
+  stop_motor_local(3);
+  delay(100);
+  motorBackward(3, MIN_SPEED);
+  delay(400);
+  stop_motor_local(3);
 }
 
 void set_kick()
 {
+  char *speed_in;
+  int speed_kick;
   
-}
-
-void set_grab()
-{
+  if(speed_in != NULL)
+  {
+    speed_kick = atof(speed_in);
+  }
+  else
+  {
+    speed_kick = MID_SPEED;
+  }
+  
+  motorForward(3, speed_kick);
+  delay(400);
+  stop_motor_local(3);
   
 }
 
