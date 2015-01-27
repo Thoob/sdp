72a8931414bceb977dc16d66ff816da1c60ad62e
Scott Postlethwaite <chinnygan@hotmail.co.uk>
2015-01-27 15:33:55 +0000

    Added Remote Control functionality to the robot

--- Arduino/RemoteControl/RemoteControl.ino
+++ Arduino/RemoteControl/RemoteControl.ino
@@ -1,6 +1,5 @@
-// Demo Code for SerialCommand Library
-// Steven Cogswell
-// May 2011
+//Remote control of robot
+//(for fun)
 
 #include "SerialCommand.h"
 #include "SDPArduino.h"
@@ -20,37 +19,28 @@ void setup() {
   Serial.begin(115200);
 
   // Setup callbacks for SerialCommand commands
-  //Test commands
   sCmd.addCommand("ON",    LED_on);          // Turns LED on
   sCmd.addCommand("OFF",   LED_off);         // Turns LED off
   sCmd.addCommand("HELLO", sayHello);        // Echos the string argument back
-  
-  //Argument commands
   sCmd.addCommand("FORWARD", move_forward);
   sCmd.addCommand("BACKWARD", move_backward);
-  sCmd.addCommand("ROTATE", move_rotate);
+  sCmd.addCommand("ROTATEL", move_rotateL);
+  sCmd.addCommand("ROTATER", move_rotateR);
   sCmd.addCommand("STOP", move_stop);
   sCmd.addCommand("KICK", move_kick);
-  
-  //Remote Control Commands
-  sCmd.addCommand("RCFORWARD", rc_forward);
-  sCmd.addCommand("RCBACKWARD", rc_backward);
-  sCmd.addCommand("RCROTATEL", rc_rotateL);
-  sCmd.addCommand("RCROTATER", rc_rotateR);
 
 
   sCmd.setDefaultHandler(unrecognized);      // Handler for command that isn't matched  (says "What?")
 
 
 
-  Serial.println("I am completely operational, and all my circuits are functioning perfectly.");
+  Serial.println("Ready");
 }
 
 void loop() {
   sCmd.readSerial();     // We don't do much, just process serial commands
 }
 
-//Test Commands
 
 void LED_on() {
   Serial.println("LED on");
@@ -74,93 +64,39 @@ void sayHello() {
   }
 }
 
-
-//Movement with argument commands
-
 void move_forward() {
   
-  char *arg1;
-  char *arg2;
-  
-  int time;
-  int power;
-  
-  arg1 = sCmd.next();
-  time = atoi(arg1);
-  
-  arg2 = sCmd.next();
-  power = atoi(arg2);
-  
-  if (time==NULL) {
-    time = 1000;
-  }
   Serial.println("Moving forward");
-  motorForward(4, power);
-  motorForward(5, power);
+  motorForward(4, 100);
+  motorForward(5, 100);
   
-  delay(time);
-  
-  motorAllStop(); 
 }
 
 void move_backward() {
   
-  char *arg;
-  int time;
-  
-  arg = sCmd.next();
-  time = atoi(arg);
-  
-  if (time==NULL) {
-    time = 1000;
-  }
   Serial.println("Moving backward");
   motorBackward(4, 100);
   motorBackward(5, 100);
   
-  delay(time);
-  
-  motorAllStop(); 
 }
 
-void move_rotate() {
-  char *arg1;
-  char *arg2;
-  char *arg3;
-  int dir;
-  int time;
-  int power;
-  
-  arg1 = sCmd.next();
-  dir = atoi(arg1);
-  
-  arg2 = sCmd.next();
-  time = atoi(arg2);
-  
-  arg3 = sCmd.next();
-  power = atoi(arg3);
-  
-  
-  
-  Serial.println("Rotating");
-  if (dir == 1) {
-  motorForward(4, power);
-  motorBackward(5, power);
-  
-    delay(time);
+void move_rotateL() {
+   Serial.println("Rotating Left");
+  motorForward(4, 100);
+  motorBackward(5, 100);
+}
+
+void move_rotateR() {
+   Serial.println("Rotating Right");
+  motorForward(5, 100);
+  motorBackward(4, 100);
+}
+
+void move_stop(){
+  Serial.println("Stopping");
   
   motorAllStop();
   
-  }
-  else if (dir==2) {
-  motorForward(5, power);
-  motorBackward(4, power);
-  
-  delay(time);
-  
-  motorAllStop();
-  
-  }
 }
 
 void move_kick() {
@@ -194,47 +130,7 @@ void move_kick() {
   motorAllStop(); 
 }
 
-void move_stop(){
-  Serial.println("Stopping");
-  
-  motorAllStop();
-  
-}
-
-
-
-//Remote Control Commands
-
-void rc_forward() {
-  
-  Serial.println("Moving forward");
-  motorForward(4, 100);
-  motorForward(5, 100);
-  
-}
-
-void rc_backward() {
-  
-  Serial.println("Moving backward");
-  motorBackward(4, 100);
-  motorBackward(5, 100);
-  
-}
-
-void rc_rotateL() {
-   Serial.println("Rotating Left");
-  motorForward(4, 100);
-  motorBackward(5, 100);
-}
-
-void rc_rotateR() {
-   Serial.println("Rotating Right");
-  motorForward(5, 100);
-  motorBackward(4, 100);
-}
-
-
 // This gets set as the default handler, and gets called when no other command matches.
 void unrecognized(const char *command) {
-  Serial.println("I'm sorry, Dave. I'm afraid I can't do that.");
+  Serial.println("What?");
 }
