dcb5dd2d212fb456a4bd42c468553830a9f7fb11
Mantas <mantasmiksys3@gmail.com>
2015-02-03 15:42:15 +0000

    minor changes

--- Java-Communication/Communication/src/com/sdp/commands/RobotCommands.java
+++ Java-Communication/Communication/src/com/sdp/commands/RobotCommands.java
@@ -3,15 +3,23 @@ package com.sdp.commands;
 import com.sdp.RobotCommunication;
 
 public class RobotCommands {
-	
-	public void changeDirection(float curDir, float toDir){
-		while(curDir<toDir){
+
+	public void changeDirection(float curDir, float toDir) {
+		while (curDir < toDir) {
 			RobotCommunication.getInstance().holdLeft();
 			curDir++;
+
 		}
+		try {
+			Thread.sleep(2000);
+			RobotCommunication.getInstance().stop();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 	}
-	
+
 	public void changePosition(float curX, float curY, float toX, float toY) {
-		
+
 	}
 }
