aed746a712e6bf49afbea85c402e0980bb44dd53
Mantas <mantasmiksys3@gmail.com>
2015-02-04 18:15:06 +0000

    added RobotCommands

---
+++ Java-Communication/Communication/src/com/sdp/planner/RobotCommands.java
@@ -0,0 +1,14 @@
+package com.sdp.planner;
+
+import com.sdp.world.DynamicWorldState.Robot;
+import com.sdp.world.Point2;
+
+public class RobotCommands {
+	public void changeHeading(Robot robot, double desiredDir) {
+
+	}
+
+	public void changePosition(Robot robot, Point2 desiredPos) {
+
+	}
+}
