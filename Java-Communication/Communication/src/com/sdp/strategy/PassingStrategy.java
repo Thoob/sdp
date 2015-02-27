82b1227a102b6629e58291ac415cb803efe650c5
Mantas Miksys <mantasmiksys3@gmail.com>
2015-02-27 11:59:13 +0000

    refactored PassingStrategy

--- Java-Communication/Communication/src/com/sdp/strategy/PassingStrategy.java
+++ Java-Communication/Communication/src/com/sdp/strategy/PassingStrategy.java
@@ -6,32 +6,28 @@ import com.sdp.world.DynamicWorldState.Robot;
 import com.sdp.world.WorldState;
 
 public class PassingStrategy extends GeneralStrategy {
-	protected boolean ballIsOnSlopeEdge;
-	protected boolean ballIsOnSideEdge;
-	protected boolean ballIsOnGoalLine;
-	protected boolean ballIsOnDefCheck;
-	protected boolean robotIsOnGoalLine;
-	protected boolean catcherIsUp = false;
-	protected boolean affectBallCaught = true;
-	protected boolean defenderHasArrived = false;
-	protected boolean defenderHasArrivedAtSafe = false;
-	protected boolean defenderIsSafe = false;
-	protected double defenderAngleToGoal;
-	protected double distFromBall;
-
+	/*
+	 * why do we need these???? they are not used anywhere... protected boolean
+	 * ballIsOnSlopeEdge; protected boolean ballIsOnSideEdge; protected boolean
+	 * ballIsOnGoalLine; protected boolean ballIsOnDefCheck; protected boolean
+	 * robotIsOnGoalLine; protected boolean catcherIsUp = false; protected
+	 * boolean affectBallCaught = true; protected boolean defenderHasArrived =
+	 * false; protected boolean defenderHasArrivedAtSafe = false; protected
+	 * boolean defenderIsSafe = false; protected double defenderAngleToGoal;
+	 * protected double distFromBall;
+	 */
 	public PassingStrategy() {
 	}
 
 	public void sendWorldState(DynamicWorldState dynWorldState,
 			WorldState worldState) {
-		if(robot==null||ball==null)return;
-		// TODO: Test! - somewhat difficulty in recognising 3 robots across the
-		// pitch
+		if (robot == null || ball == null)
+			return;
 		// ROBOT DECLARATIONS //
 		Robot attackingRobot = dynWorldState.getAttacker();
 		Robot enemyDefender = dynWorldState.getEnemyDefender();
 
-		// ROBOT COORDANATES //
+		// ROBOT COORDINATES //
 		double attackerX = attackingRobot.getCenter().getX();
 		double attackerY = attackingRobot.getCenter().getY();
 		double enemyDefenderX = enemyDefender.getCenter().getX();
@@ -44,31 +40,36 @@ public class PassingStrategy extends GeneralStrategy {
 				robotAngleRad, enemyDefenderX, enemyDefenderY);
 		double diffInHeadingsToAttacker = Math.abs(robotAngleDeg
 				- attackerAngle);
-		double diffInHeadingstoBlocker = Math.abs(robotAngleDeg
+		double diffInHeadingsToBlocker = Math.abs(robotAngleDeg
 				- enemyDefenderAngle);
 
 		// STATE BOOLEANS //
 		/* We are currently facing the attacker */
-		boolean facingAttacker = (diffInHeadingsToAttacker < allowedDegreeError || diffInHeadingsToAttacker > 360 - allowedDegreeError);
+		boolean facingAttacker = isFacingAttacker(diffInHeadingsToAttacker);
 		/* The Blocker is currently outwith our facing angle */
-		boolean noObstruction = (diffInHeadingstoBlocker > allowedDegreeError || diffInHeadingstoBlocker < 360 - allowedDegreeError);
+		boolean enemyBlocking = isEnemyBlocking(diffInHeadingsToBlocker);
 		/* We are facing the attacker with no obstruction */
-		boolean inLineOfSight = (facingAttacker && noObstruction);
+		boolean attackerInLineOfSight = (facingAttacker && enemyBlocking);
 
 		System.out.println("Attacker " + attackerX + " " + attackerY
 				+ " Heading diff: " + diffInHeadingsToAttacker);
 		System.out.println("Blocker " + enemyDefenderX + " " + enemyDefenderY);
 
 		/* DEBUG */
-		if (!noObstruction) {
-			System.out.println("Enemy is blocking");
-		}
-		if (facingAttacker) {
-			System.out.println("We are facing Attacker");
-		}
-		if (inLineOfSight) {
+		if (attackerInLineOfSight)
 			System.out.println("Attacker is in line of sight");
-		}
+		else if (enemyBlocking)
+			System.out.println("Enemy is blocking");
+		else if (facingAttacker)
+			System.out.println("We are facing Attacker");
 
+	}
+
+	private boolean isEnemyBlocking(double diffInHeadingsToBlocker) {
+		return !(diffInHeadingsToBlocker > allowedDegreeError || diffInHeadingsToBlocker < 360 - allowedDegreeError);
+	}
+
+	private boolean isFacingAttacker(double diffInHeadingsToAttacker) {
+		return (diffInHeadingsToAttacker < allowedDegreeError || diffInHeadingsToAttacker > 360 - allowedDegreeError);
 	}
 }
